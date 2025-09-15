82e3f7b0e3c3d9abeafb93051e883930e4345422
Iris Perry <irisperry489@gmail.com>
2025-09-14 22:30:43 -0500

    Base program setup, clean up graphs

--- main.py
+++ main.py
@@ -11,7 +11,7 @@
 #     (5/5 points) Initial comments with your name, class and project at the top of your .py file.
 #     (5/5 points) Proper import of packages used.
 #     (20/20 points) Using an API of your choice (yfinance works), collect the closing price of 5 of your favorite stock tickers for the last 10 trading days.
-#     (10/10 points) Store this information in a list that you will convert to a array in NumPy.
+#     (10/10 points) Store this information in a list that you will convert to an array in NumPy.
 #     (10/10 points) Plot these 5 graphs. Feel free to add as much information to the graphs as you like exploring the documentation for matplotlib. At minimum it just needs to show 10 data points.
 #     (10/10 points) Save these graphs in a folder called charts as PNG files. Do not upload these to your project folder, the project should save these when it executes. You may want to add this folder to your .gitignore file.
 #     (10/10 points) There should be a minimum of 5 commits on your project, be sure to commit often!
@@ -22,6 +22,7 @@ import yfinance as yf
 import matplotlib.pyplot as plt
 import numpy as np
 import pprint
+import copy
 
 mystocks = ['MSFT', 'AAPL', 'GOOG', 'NVDA', 'WMT']
 mystockdata = {}
@@ -29,6 +30,20 @@ mystockdata = {}
 for stock in mystocks:
     dat = yf.Ticker(stock)
     last10 = dat.history(period='10d')
-    mystockdata[stock] = last10['Close']
+    mystockdata[stock] = []
 
-pprint.pprint(mystockdata)
+    # Create a list of Closing Prices
+    for price in last10['Close']:
+        mystockdata[stock].append(price)
+    mystock = np.array(mystockdata[stock])
+
+    # Create variable to determine HIGH and LOW in time period
+    hl = copy.copy(mystockdata[stock])
+    hl.sort()
+
+    # Generate graphs
+    plt.plot(mystock)
+    plt.axis((0, 10, hl[0]-10, hl[-1]+10))
+    plt.ylabel('Closing Price')
+    plt.xlabel('Trading Days Ago')
+    plt.show()
