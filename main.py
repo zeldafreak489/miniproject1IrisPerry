c2f95794fe1255c96f76acccbd9bb9d96b7e711a
Iris Perry <irisperry489@gmail.com>
2025-09-14 22:40:59 -0500

    Added charts folder and put in gitignore. Added feature to save charts

--- main.py
+++ main.py
@@ -12,7 +12,7 @@
 #     (5/5 points) Proper import of packages used.
 #     (20/20 points) Using an API of your choice (yfinance works), collect the closing price of 5 of your favorite stock tickers for the last 10 trading days.
 #     (10/10 points) Store this information in a list that you will convert to an array in NumPy.
-#     (10/10 points) Plot these 5 graphs. Feel free to add as much information to the graphs as you like exploring the documentation for matplotlib. At minimum it just needs to show 10 data points.
+#     (10/10 points) Plot these 5 graphs. Feel free to add as much information to the graphs as you like exploring the documentation for matplotlib. At minimum, it just needs to show 10 data points.
 #     (10/10 points) Save these graphs in a folder called charts as PNG files. Do not upload these to your project folder, the project should save these when it executes. You may want to add this folder to your .gitignore file.
 #     (10/10 points) There should be a minimum of 5 commits on your project, be sure to commit often!
 #     (10/10 points) I will be checking out the main branch of your project. Please be sure to include a requirements.txt file which contains all the packages that need installed. You can create this file with the output of pip freeze at the terminal prompt.
@@ -21,7 +21,6 @@
 import yfinance as yf
 import matplotlib.pyplot as plt
 import numpy as np
-import pprint
 import copy
 
 mystocks = ['MSFT', 'AAPL', 'GOOG', 'NVDA', 'WMT']
@@ -43,7 +42,9 @@ for stock in mystocks:
 
     # Generate graphs
     plt.plot(mystock)
+    plt.title(stock)
     plt.axis((0, 10, hl[0]-10, hl[-1]+10))
     plt.ylabel('Closing Price')
     plt.xlabel('Trading Days Ago')
+    plt.savefig(f'charts/{stock}.png')
     plt.show()
